db79b293ffb84b94cf07973912eff69e586636e3
kondepudi Geetanjali <65365851+Geetanjali1129@users.noreply.github.com>
2021-06-26 10:44:56 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,15 @@
+public class EvenorOdd
+{
+public static void main(String[] args)
+{
+int num=3;
+if((num&1)==0)
+{
+System.out.println(num+"is an even number");
+}
+else
+{
+System.out.println(num+"is an odd number");
+}
+}
+}
